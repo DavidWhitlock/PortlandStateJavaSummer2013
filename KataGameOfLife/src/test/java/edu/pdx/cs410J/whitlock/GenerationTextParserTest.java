fe1e987a8b37591922e3debdf5a0f8b43a6d4536
David Whitlock <notmyrealaddress@email.com>
2013-07-31 19:33:40 -0700

    Test a generation with both alive and dead cells.

--- KataGameOfLife/src/test/java/edu/pdx/cs410J/whitlock/GenerationTextParserTest.java
+++ KataGameOfLife/src/test/java/edu/pdx/cs410J/whitlock/GenerationTextParserTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.StringReader;
 
+import static edu.pdx.cs410J.whitlock.CellState.ALIVE;
+import static edu.pdx.cs410J.whitlock.CellState.DEAD;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -44,13 +46,35 @@ public class GenerationTextParserTest {
       "....",
       "...."
     );
-    assertThatAllCellsHaveState(generation, CellState.DEAD);
+    assertThatAllCellsHaveState(generation, DEAD);
+  }
+
+  @Test
+  public void someDeadCellsSomeAliveCells() throws IOException {
+    Generation generation = parseGenerationText(
+      ".*.",
+      "*.*",
+      ".*."
+    );
+    assertThatCellHasState(generation, 0, 0, DEAD);
+    assertThatCellHasState(generation, 0, 1, ALIVE);
+    assertThatCellHasState(generation, 0, 2, DEAD);
+    assertThatCellHasState(generation, 1, 0, ALIVE);
+    assertThatCellHasState(generation, 1, 1, DEAD);
+    assertThatCellHasState(generation, 1, 2, ALIVE);
+    assertThatCellHasState(generation, 2, 0, DEAD);
+    assertThatCellHasState(generation, 2, 1, ALIVE);
+    assertThatCellHasState(generation, 2, 2, DEAD);
+  }
+
+  private void assertThatCellHasState(Generation generation, int row, int column, CellState state) {
+    assertThat(generation.getCellState(row, column), equalTo(state));
   }
 
   private void assertThatAllCellsHaveState(Generation generation, CellState state) {
     for (int row = 0; row < generation.getNumberOfRows(); row++) {
       for (int column = 0; column < generation.getNumberOfColumns(); column++) {
-        assertThat(generation.getCellState(row, column), equalTo(state));
+        assertThatCellHasState(generation, row, column, state);
       }
     }
   }
